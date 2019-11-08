dddf9887f28e8bb8f9c385ef59075dfac85546cc
kamyranwilliams <37843545+kamyranwilliams@users.noreply.github.com>
2019-11-08 03:52:16 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// JavaScript popup window function
+function makeopaque(elem){
+	elem.classList.add("opaque");
+}
+	function basicPopup(url) {
+popupWindow = window.open(url,'popUpWindow','height=300,width=700,left=50,top=50,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes')
+	}
+/*having popup window issues + */
+
+/* or for a centered popup
+var popupWindow = null;
+function centeredPopup(url,winName,w,h,scroll){
+LeftPosition = (screen.width) ? (screen.width-w)/2 : 0;
+TopPosition = (screen.height) ? (screen.height-h)/2 : 0;
+settings =
+'height='+h+',width='+w+',top='+TopPosition+',left='+LeftPosition+',scrollbars='+scroll+',resizable'
+popupWindow = window.open(url,winName,settings) 
+HTML
+<p><a href="https://www.quackit.com/javascript/examples/sample_popup.cfm" onclick="centeredPopup(this.href,'myWindow','700','300','yes');return false">text</a></p>*/
