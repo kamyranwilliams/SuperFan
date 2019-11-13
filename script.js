a97d35c268c12f0c6a0259efc6443fd5d48494f1
kamyranwilliams <37843545+kamyranwilliams@users.noreply.github.com>
2019-11-13 14:42:45 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+//JQuery code for images to fit a div
+$(document).ready(function(){
+    imageWidth = $('.column_b img').width();
+    parentWidth = $('.main').width();
+    if (imageWidth > parentWidth) {
+        $('.column_b').css('width', '100%');
+    }
+});
 
-// JavaScript popup window function
 function makeOpaque(elem) {
 	elem.classList.add("opaque");
 }
+// JavaScript popup window function
 	function basicPopup(url) {
 popupWindow = window.open(url,'popUpWindow','height=300,width=700,left=50,top=50,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes')
 	}
